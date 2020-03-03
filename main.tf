63ef3f223a4ac4b88d038b866bfdcdf36b5bf14d
Cas <cas.thomas.r@gmail.com>
2020-03-03 11:11:32 -0500

    Remove AnyAny 80 rule from lb-vault-sg

--- main.tf
+++ main.tf
@@ -213,13 +213,6 @@ resource "aws_security_group" "lb-vault-sg" {
   tags        = local.sg_tags
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
